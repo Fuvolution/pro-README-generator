6705eafab2e4f23c241c1a1d4e76d6a8c49e99ea
Phuc Dinh <ams.dinhphuc@gmail.com>
2024-01-29 16:54:55 -0600

    Added changes that enable this application to functionally generate a README file based on user input!

---
+++ dist/README.md
@@ -0,0 +1,21 @@
+# Professional README generator
+![Github license](https://img.shields.io/badge/license-ISC-blue.svg)
+## Description
+This project takes in user answers to the provided questions to generate a README file!
+## Table of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributions)
+* [Tests](#testing)
+* [Questions](#questions)
+## Installation
+You need to install Inquirer!
+## Usage
+I used Inquirer!
+## Contributing
+There are no guidelines!
+## Tests
+You need to open the Integrated Terminal, do npm install, and then run node index.js to begin testing my project!
+## Questions
+Please send your questions to Fuvolution [Here](mailto:ams.dinhphuc@gmail.com)
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,55 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license !== "none") {
+    return `![Github license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  }
+  return "";
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// This function returns the license link
+// If there is no license, this function will return an empty string
+function renderLicenseLink(license) {
+  if(license !== "none"){
+    return `\n* [License](#license)\n`;
+  }
+  return "";
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// This function returns the license section of README
+// If there is no license, this function will return an empty string
+function renderLicenseSection(license) {
+  if(license !== "none"){
+    return `## License
+    
+        Licensed under the ${license} license.`;
+  }
+  return "";
+}
 
-// TODO: Create a function to generate markdown for README
+// This function generates the markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+${renderLicenseBadge(data.license)}
+## Description
+${data.description}
+## Table of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributions)
+* [Tests](#testing)
+* [Questions](#questions)
+## Installation
+${data.installation}
+## Usage
+${data.usage}
+## Contributing
+${data.contributions}
+## Tests
+${data.testing}
+## Questions
+Please send your questions to ${data.username} [Here](mailto:${data.email})
 `;
 }
 
